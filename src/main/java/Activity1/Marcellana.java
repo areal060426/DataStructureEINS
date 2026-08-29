d15552c0f0cd18a4008e9ac5aaffb44e84a849c7
ITSNicole14 <marcellananicole30@gmail.com>
2026-08-29 18:20:55 +0800

    Add Activity

---
+++ src/main/java/Activity1/Marcellana.java
@@ -0,0 +1,34 @@
+package Activity1;
+
+public class Marcellana {
+	public static void main( String [] args) {
+		
+		String fname = "Princess Nicole M. Marcellana";
+		String age = "CONFIDENTIAL (I'd rather not say.)";
+		String yearlvl = "2nd Year";
+		String course = "Data Structure";
+		String school = "University of Makati";
+		String hobby1 = "Playing Online Games";
+		String hobby2 = "Listening to Music";
+		String hobby3 = "Playing Badminton, Volleyball, and Table Tennis";
+		String fplt = "Java and HTML";
+		String why = "I want to improve my programming skills and learn how to create different programs.";
+
+
+
+
+		
+		System.out.println("---------- My Personal Information ---------- \n");
+		System.out.println("Full Name: \n " + fname + "\n");
+		System.out.println("Age: \n " + age + "\n");
+		System.out.println("Course and Year Level: \n " + yearlvl + ", " + course + "\n");
+		System.out.println("School: \n " + school + "\n");
+		System.out.println("Hobbies: \n - " 
+				+ hobby1 + "\n - " 
+				+ hobby2 + "\n - " 
+				+ hobby3 + "\n");
+		System.out.println("Favorite Programming Language or Technology: \n " + fplt + "\n");
+		System.out.println("Why do you want to learn Java?: \n " + why);
+	}
+
+}
