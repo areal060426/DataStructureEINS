a8a65544a1768a0b9fa0e480fcf33fd39581601b
Kurt Russel Balagtas <kbalagtas.1583@umak.edu.ph>
2026-08-29 19:19:07 +0800

    Activity #1
    Submitted Activity

---
+++ src/main/java/Activity1/Balagtas.java
@@ -0,0 +1,26 @@
+package Activity1;
+
+public class Balagtas {
+
+	public static void main(String[] args) {
+		
+		String Name = "Kurt Russel Balagtas";
+		int Age = 19;
+		String Course = "Data Structure";
+		String Year = "2nd Year";
+		String School = "University of Makati";
+		String Hobbies = "Volleyball and Billiards";
+		String Language = "Java";
+		
+		System.out.println("\n------- Activity 1 -------\n");
+		System.out.println("Name: " + Name);
+		System.out.println("Age: " + Age);
+		System.out.println("Course: " + Course);
+		System.out.println("Year Level: " + Year);
+		System.out.println("School: " + School);
+		System.out.println("Hobbies: " + Hobbies);
+		System.out.println("Programing Language: " + Language);
+		System.out.println("I want to learn Java so that I can use it in my future job");
+	}
+
+}
