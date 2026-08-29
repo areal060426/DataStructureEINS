4a40252fb594c53152515720f983e597bf8851db
Daren <darensimplina3@gmail.com>
2026-08-29 19:55:14 +0800

    Create Simplina.java
    Activity 1

---
+++ src/main/java/Activity1/Simplina.java
@@ -0,0 +1,22 @@
+package Activity1;
+
+public class Simplina {
+	public static void main(String[] args) {
+
+        String fullName = "DAREN SIMPLINA";
+        int age = 22;
+        String course = "BS Information Technology - 2nd Year";
+        String school = "University of Makati";
+        String hobbies = "Playing games, listening to music, and cooking";
+        String favoriteLanguage = "Java & C++";
+        String reason = "I want to learn Java and C++ to improve my programming skills and understand how to create useful applications.";
+
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Age: " + age);
+        System.out.println("Course and Year Level: " + course);
+        System.out.println("School: " + school);
+        System.out.println("Hobbies: " + hobbies);
+        System.out.println("Favorite Programming Language is " + favoriteLanguage);
+        System.out.println("Reason why I want to learn Java: " + reason);
+    }
+}
