364e811ad10746352d2b399ed8b567963f4e759f
Shuurimp <jegan.2740@umak.edu.ph>
2026-08-30 14:15:34 +0800

    Create Egan.java

---
+++ src/main/java/Activity1/Egan.java
@@ -0,0 +1,22 @@
+package Activity1;
+
+public class Egan {
+    public static void main(String[] args) {
+        
+        String FName = "Jerryzon Egan";
+        int Age = 28;
+        String Course = "BSIT";
+        String YearLevel = "2nd Year";
+        String School = "Makati University";
+        String Hobby = "playing online games";
+        String FavLang = "Java";
+        String Reason = "I want to learn Java because it can help me improve my programming skills "
+                + "and understand how to create useful programs and applications.";
+        
+        System.out.println("Hello, my name is " + FName + ".");
+        System.out.println("I am " + Age + " years old.");
+        System.out.println("I am currently in my " + YearLevel + " taking " + Course + " at the " + School + ".");
+        System.out.println("My hobby is " + Hobby + ".");
+        System.out.println("Right now, my favorite language is " + FavLang + " because " + Reason);
+    }
+}
