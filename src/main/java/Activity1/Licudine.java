d5f3d4840499c7d889138e972591ac86a9dc474a
Yvancodes <yvanlicudine@gmail.com>
2026-08-31 17:26:50 +0800

    Activity 1 done

---
+++ src/main/java/Activity1/Licudine.java
@@ -0,0 +1,14 @@
+package Activity1;
+
+public class Licudine {
+    public static void main(String[] args) {
+       
+        System.out.println("Name: James Yvan B. Licudine");
+        System.out.println("Age: 19");
+        System.out.println("Course and Year Level: BS Information Technology specialized in Network Security - 2nd Year");
+        System.out.println("School: University of Makati");
+        System.out.println("Hobbies: Playing Mobile Games and Watching Movies");
+        System.out.println("Favorite Programming Language or Technology: Java");
+        System.out.println("Reason why you want to learn Java: I want to understand how cryptographic functions and data integrity checks are built into software to prepare for more advanced concepts in network security.");
+    }
+}
