80547fbe02473e529d0a845e416efebbef003de1
Kurt Russel Balagtas <kbalagtas.1583@umak.edu.ph>
2026-09-16 15:37:14 +0800

    Tinitigan
    act 1

---
+++ src/main/java/Activity1/Tinitigan.java
@@ -0,0 +1,28 @@
+package Activity1;
+
+
+
+public class Tinitigan {
+
+	public static void main(String[] args) {
+		
+
+		String Name = "Tinitigan, Reden V.";
+		int Age = 21;
+		String Course = "Data Structure";
+		String Year = "2nd Year";
+		String School = "University of Makati";
+		String Hobbies = "Cooking and Running";
+		String Language = "Java";
+		
+		System.out.println("\n------- Activity 1 -------\n");
+		System.out.println("Name: " + Name);
+		System.out.println("Age: " + Age);
+		System.out.println("Course: " + Course);
+		System.out.println("Year Level: " + Year);
+		System.out.println("School: " + School);
+		System.out.println("Hobbies: " + Hobbies);
+		System.out.println("Programing Language: " + Language);
+		System.out.println("I want to learn Java so that I can use it in my work");	}
+
+}
