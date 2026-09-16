ce7e5dcaf79a9ad38a48d182eea040601d2592aa
Rafael Jr. Canalda <147781291+raflearns@users.noreply.github.com>
2026-09-16 14:08:08 +0800

    Create Dariooo.java

---
+++ src/main/java/Activity1/Dariooo.java
@@ -0,0 +1,13 @@
+package Activity1;
+
+public class Dariooo {
+	public static void main(String[] args) {
+		System.out.println("Dario, Jerem");
+		System.out.println("30");
+		System.out.println("BSIT II-EINS");
+		System.out.println("University of Makati");
+		System.out.println("Programming, Eletronics, Gaming, Reading, Binge Watching, Billards");
+		System.out.println("Python and Linux");
+		System.out.println("To build applications and play mobile legends all day.");
+	}
+}
