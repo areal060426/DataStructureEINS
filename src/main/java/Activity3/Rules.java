1874e20120982b79071bbc7f90dd2174374743cd
areal060426 <areal@umak.edu.ph>
2026-09-16 14:11:16 +0800

    game

---
+++ src/main/java/Activity3/Rules.java
@@ -0,0 +1,16 @@
+package Activity3;
+
+public class Rules {
+/*
+No Arrays.sort().
+Must use Scanner.
+Must use at least one array.
+Group 4 must use a 2D array.
+Students must manually implement sorting.
+Program must not crash when invalid menu options are entered.
+Sorting must not destroy related information.
+Students must explain their sorting algorithm during presentation.
+Each member must explain at least one portion of the code.
+Instructor may modify one input during demonstration to verify that the program is not hard-coded.
+*/
+}
